d0d7aec555b95520e2c493c719785a81c9811c02
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-13 10:10:36 +0700

    membuat kondisi jika start == null atau noMhs node baru <=noMhs start

--- 0222_Doblelinkedlist.cpp
+++ 0222_Doblelinkedlist.cpp
@@ -16,4 +16,18 @@ Node*START = NULL;
 // deklarasi prosedur addnode 
 void addNode(){
     Node *newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+
+    // insert the new node in the list 
+    //kondisi jika start == null atau noMhs node baru <=noMhs star
+
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        // step 2: insert the new node at the beginning 
+        //kondisi jika start tidak kosong dan noMhs node baru sama dengan noMhs
+    }
+    
 }
