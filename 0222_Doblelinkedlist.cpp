049bad7450f9107492ee27ad04a813b7bdba0e1e
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-13 10:12:54 +0700

    membuat kondisi jika start tidak kosong dan noMhs node baru sama dengan noMhs

--- 0222_Doblelinkedlist.cpp
+++ 0222_Doblelinkedlist.cpp
@@ -22,12 +22,17 @@ void addNode(){
     cin >> newNode->name;
 
     // insert the new node in the list 
-    //kondisi jika start == null atau noMhs node baru <=noMhs star
+    //kondisi jika start == null atau noMhs node baru <=noMhs start
 
     if (START == NULL || newNode->noMhs <= START->noMhs)
     {
         // step 2: insert the new node at the beginning 
         //kondisi jika start tidak kosong dan noMhs node baru sama dengan noMhs
+        if (START != NULL && newNode ->noMhs == START->noMhs)
+        {
+            /* code */
+        }
+        
     }
     
 }
