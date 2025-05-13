156ce039560edfba8b4a8283166196dcdd3b8989
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-13 10:43:20 +0700

    membuat kondisi jika previous tidka sama dengan null

--- 0222_Doblelinkedlist.cpp
+++ 0222_Doblelinkedlist.cpp
@@ -7,50 +7,50 @@ struct Node
     int noMhs;
     string name;
 
-    Node*next;
-    Node*prev;
+    Node *next;
+    Node *prev;
 };
 
-Node*START = NULL;
+Node *START = NULL;
 
-// deklarasi prosedur addnode 
-void addNode(){
+// deklarasi prosedur addnode
+void addNode()
+{
     Node *newNode = new Node();
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs;
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;
 
-    // insert the new node in the list 
-    //kondisi jika start == null atau noMhs node baru <=noMhs start
+    // insert the new node in the list
+    // kondisi jika start == null atau noMhs node baru <=noMhs start
 
     if (START == NULL || newNode->noMhs <= START->noMhs)
     {
-        // step 2: insert the new node at the beginning 
-        //kondisi jika start tidak kosong dan noMhs node baru sama dengan noMhs
-        if (START != NULL && newNode ->noMhs == START->noMhs)
+        // step 2: insert the new node at the beginning
+        // kondisi jika start tidak kosong dan noMhs node baru sama dengan noMhs
+        if (START != NULL && newNode->noMhs == START->noMhs)
         {
             cout << "\033[31mDuplicate roll numbers not allowe\033[0m" << endl;
             return;
         }
         // jika list kosong, maka node text nya adalah START
         newNode->next = START;
-        // kondisi jika start tidak memiliki nilai atau tidak kosong 
+        // kondisi jika start tidak memiliki nilai atau tidak kosong
         if (START != NULL)
         {
-            START->prev = newNode; 
+            START->prev = newNode;
         }
         newNode->prev = NULL;
         START = newNode;
-        
     }
-    //kondisi jika semua kondisi if tidak terpenuhi
+    // kondisi jika semua kondisi if tidak terpenuhi
     else
     {
 
         Node *current = START;
         Node *previous = NULL;
-        
+
         while (current != NULL && current->noMhs < newNode->noMhs)
         {
             previous = current;
@@ -59,6 +59,17 @@ void addNode(){
 
         newNode->next = current;
         newNode->prev = previous;
+    }
+
+    // kondisi jika current tidak sama dengan null
+    if (current != NULL)
+    {
+        current->prev = newNode;
+    }
+
+    if (previous != NULL)
+    {
+        previous->next = newNode;
     }
     
 }
